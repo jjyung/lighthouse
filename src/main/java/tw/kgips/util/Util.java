6a03d2f05646af42906502e36d7c813b8100811c
Samson <jjyung1234@gmail.com>
2020-12-08 16:00:17 +0800

    change cert for 期交所證交所

--- src/main/java/tw/kgips/util/Util.java
+++ src/main/java/tw/kgips/util/Util.java
@@ -12,41 +12,48 @@ import java.util.Objects;
 
 public class Util {
 
-	public static String getJSON(String url, int timeout) throws IOException {
+    private Util() {
+    }
 
-		URL u = new URL(url);
-		HttpURLConnection c = (HttpURLConnection) u.openConnection();
+    public static String getJSON(String url, int timeout) throws IOException {
 
-		c.setRequestMethod("GET");
-		c.setUseCaches(false);
-		c.setAllowUserInteraction(false);
-		c.setConnectTimeout(timeout);
-		c.setReadTimeout(timeout);
-		c.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
-		c.connect();
+        URL u = new URL(url);
+        HttpURLConnection c = (HttpURLConnection) u.openConnection();
 
-		int status = c.getResponseCode();
+        c.setRequestMethod("GET");
+        c.setUseCaches(false);
+        c.setAllowUserInteraction(false);
+        c.setConnectTimeout(timeout);
+        c.setReadTimeout(timeout);
+        c.setRequestProperty("User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
+        c.connect();
 
-		switch (status) {
-			case 200:
-			case 201:
-				BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream(), StandardCharsets.UTF_8));
-				StringBuilder sb = new StringBuilder();
-				String line;
-				while ((line = br.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-				br.close();
-				return sb.toString();
-		}
+        int status = c.getResponseCode();
 
-		return null;
-	}
+        switch (status) {
+            case 200:
+            case 201:
+                BufferedReader br = new BufferedReader(
+                    new InputStreamReader(c.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                br.close();
+                return sb.toString();
+            default:
+                throw new RuntimeException("fail to get the response");
+        }
 
-	public static void setSSL() {
-		String certFileName = "tpex.taifex.twse.com.tw.jks";
-		ClassLoader classLoader = FutureExchange.class.getClassLoader();
-		System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(classLoader.getResource(certFileName)).getPath());
-	}
+    }
+
+    public static void setSSL() {
+        String certFileName = "client.jks";
+        ClassLoader classLoader = FutureExchange.class.getClassLoader();
+        System.setProperty("javax.net.ssl.trustStore",
+            Objects.requireNonNull(classLoader.getResource(certFileName)).getPath());
+    }
 
 }
